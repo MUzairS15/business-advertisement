d4a398b66ad8fd8c4f0b89ca7b79a6e027b3c7ff
MUzairS <muzair.shaikh810@gmail.com>
2021-08-03 13:57:03 +0530

    Basic Structure

---
+++ static/data.js
@@ -0,0 +1,7 @@
+
+
+
+document.getElementById("cards").insertAdjacentHTML("afterbegin",
+```<h1>Name: ${data[0].name} Business: ${data[0].business} Phone: ${data[0].phone}/h1>```);
+
+console.log("GGG");
