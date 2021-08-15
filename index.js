af58a7cd056ce7ceb622de8a60e4fc001d90b0b0
MUzairS <muzair.shaikh810@gmail.com>
2021-08-15 11:28:57 +0530

    Database connection

--- index.js
+++ index.js
@@ -4,42 +4,42 @@ const path = require('path')
 bodyParser = require('body-parser');
 
 
-// mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
-//  const { Schema } = mongoose;
-
-const app = express();
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+ const { Schema } = mongoose;
+const {ObjectId} = require('mongodb');
+  const app = express();
 const port = 8080;
-// const db = mongoose.connection;
+const db = mongoose.connection;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname + "/static")));
 
 app.set('view engine', 'pug');
-// const detailSchema = new Schema({
-//     name: String,
-//     business: String,
-//     email: String,
-//     phone: String,
-//     more: String
-// });
-// const info = mongoose.model('Info',detailSchema);
+const detailSchema = new Schema({
+    name: String,
+    business: String,
+    email: String,
+    phone: String,
+    more: String
+});
+const info = mongoose.model('Info',detailSchema,'info');
 
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function() {
-//   console.log("Connection Successful....")
-// });
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Connection Successful....")
+});
 
 app.get('/', function (req, res) {
     console.log("Hi");
 
 
-    // info.find({},{_id:0}).sort({$natural:-1}).limit(1)
-    // .exec(function (err, doc) {
-    //       if (err) { return (err); }
-         
-    res.render('index', { 'data':[{"name":"Uzair", "phone":0998}] });
+    info.find({}).sort({$natural:-1})
+    .exec(function (err, doc) {
+          if (err) { return (err); }
+        //  console.log(doc._id);
+    res.render('index', { 'data':doc });
 
-    // });
+    });
     
 });
 
@@ -48,10 +48,17 @@ app.get('/data', function (req, res) {
     res.render('data')
 })
 
-app.get('/update',function(req,res){
-    const id =  req.body.update;
+app.get('/update/:id',function(req,res){
+    const id =  req.params.id;
+    _id = ObjectId(id);
+    info.findById(_id)
+    .exec(function (err, doc) {
+          if (err) { return (err); }
+        //  console.log(doc.name);
+    res.render('update',{'data':doc});
+
+    }); 
     // console.log(id)
-    res.render('update')
 
 })
 app.post('/delete',function(req,res){
@@ -63,8 +70,16 @@ app.post('/', function (req, res) {
        res.redirect('/data');
      
     });
-app.post('/update', function(req, res){
-    res.render('/');
+app.post('/update/:id', function(req, res){
+
+    id = req.params.id 
+    info.findByIdAndUpdate({_id:Object(id)}, req.body, useFindAndModify= false,function(result, err){
+            if(err){
+                return (err);
+            }
+    })
+    
+    res.redirect('/');
 })
 
 app.post('/data', function (req, res) {
@@ -75,12 +90,21 @@ app.post('/data', function (req, res) {
         if(err) throw err;
         if(result){
     res.redirect("/");
-    console.log(req.body.name)
+    
     }
+    // console.log(data1.id)
     });
 
 });
-
+app.get('/delete/:id',function(req,res){
+id = req.params.id;
+console.log(id)
+    info.deleteOne({_id:Object(id)},function(result, err){
+        if(err)
+        return err;
+    })
+    res.redirect('/');
+})
 app.listen(port, function () {
     console.log(`Server started successfully on port ${port} ....`);
 });
